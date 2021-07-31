558ce01f69209519e52b3309a54cff4dc33a5d68
CrazyTim <impdeveloper@gmail.com>
2021-07-31 21:35:17 +1000

    Refactor: replace lots of code with regexp

--- FileMakerOdbcDebugger.Util.Tests/Sql/EscapeIdentifiyersStartingWithUnderscore.cs
+++ FileMakerOdbcDebugger.Util.Tests/Sql/EscapeIdentifiyersStartingWithUnderscore.cs
@@ -7,19 +7,21 @@ namespace FileMakerOdbcDebugger.Util.Tests
     {
         public class EscapeIdentifiyersStartingWithUnderscore
         {
-            public static IEnumerable<object[]> TestData = new List<object[]>()
+            public static IEnumerable<object[]> Tests = new List<object[]>()
             {
                 new object[] { '\r' + "_test" + '\r', '\r' + "\"_test\"" + '\r' },
                 new object[] { '\t' + "_test" + '\t', '\t' + "\"_test\"" + '\t' },
+                new object[] { '\n' + "_test" + '\n', '\n' + "\"_test\"" + '\n' },
             };
 
             [Theory]
             [InlineData("_test", "\"_test\"")]
             [InlineData(" _test ", " \"_test\" ")]
-            [InlineData("test_test", "test_test")]
+            [InlineData("_test _test", "\"_test\" \"_test\"")] // multiple words
+            [InlineData("test_test", "test_test")] // underscore in the middle of a word
             [InlineData(" test_test ", " test_test ")]
-            [MemberData(nameof(TestData))]
-            public void Identifiers_starting_with_an_underscore_are_escaped(string query, string expectedResult)
+            [MemberData(nameof(Tests))]
+            public void Words_starting_with_an_underscore_are_escaped(string query, string expectedResult)
             {
                 // Act
                 var result = Util.Sql.EscapeIdentifiersStartingWithUnderscore(query);
--- FileMakerOdbcDebugger.Util/Sql/EscapeIdentifiyersStartingWithUnderscore.cs
+++ FileMakerOdbcDebugger.Util/Sql/EscapeIdentifiyersStartingWithUnderscore.cs
@@ -1,66 +1,18 @@
-using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace FileMakerOdbcDebugger.Util
 {
     public static partial class Sql
     {
-        public static readonly char[] IdentifierStartCharacters = { ' ', ',', '(', '.', '\t', '\r', '\n', '=' };
-        public static readonly char[] IdentifierEndCharacters = { ' ', ',', ')', '.', '\t', '\r', '\n', '=' };
+        private static readonly Regex findIdentifiersStartingWithUnderscore = new Regex(@"(?<=[\s,""(\.=]|^)(_.*?)(?=[\s,"")\.=]|$)");
 
         /// <summary>
-        /// Insert double-quotes around identifiyers starting with "_" (which is illegal in odbc, but not for FileMaker).
-        /// In FileMaker its common to name fields starting with a underscore (_).
+        /// Insert double-quotes around identifiyers starting with an underscore ("_") (which is illegal for OBDC, but not FileMaker).
+        /// Note: in FileMaker its common to name fields starting with a underscore.
         /// </summary>
-        public static string EscapeIdentifiersStartingWithUnderscore(string sqlStatement)
+        public static string EscapeIdentifiersStartingWithUnderscore(string s)
         {
-            if (string.IsNullOrEmpty(sqlStatement)) return "";
-
-            bool isWordFound = false;
-            int startIndexOfWord = -1;
-            int endIndexOfWord;
-
-            // Test first char:
-            if (sqlStatement[0] == '_')
-            {
-                isWordFound = true;
-                startIndexOfWord = 0;
-            }
-
-            // Test middle chars:
-            int i = 1;
-            while (i <= sqlStatement.Length - 1)
-            {
-                var thisChar = sqlStatement[i];
-                var prevChar = sqlStatement[i - 1];
-
-                if (!isWordFound)
-                {
-                    if (thisChar == '_' & IdentifierStartCharacters.Contains(prevChar))
-                    {
-                        isWordFound = true;
-                        startIndexOfWord = i;
-                    }
-                }
-                else if (IdentifierEndCharacters.Contains(thisChar))
-                {
-                    endIndexOfWord = i;
-                    sqlStatement = sqlStatement.Insert(endIndexOfWord, "\"");
-                    sqlStatement = sqlStatement.Insert(startIndexOfWord, "\"");
-                    i += 2;
-                    isWordFound = false;
-                }
-                i += 1;
-            }
-
-            // Test last char:
-            if (isWordFound)
-            {
-                endIndexOfWord = sqlStatement.Length;
-                sqlStatement = sqlStatement.Insert(endIndexOfWord, "\"");
-                sqlStatement = sqlStatement.Insert(startIndexOfWord, "\"");
-            }
-
-            return sqlStatement;
+            return findIdentifiersStartingWithUnderscore.Replace(s, m => '"' + m.ToString() + '"');
         }
     }
 }
